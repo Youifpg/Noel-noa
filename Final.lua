f31eca2a100e1c42538fd44c9c42953ca4c4ba8f
ARBIX <youssefalsadick@gmail.com>
2025-02-11 00:10:27 +0300

    Update Final.lua

--- Final.lua
+++ Final.lua
@@ -5,5 +5,5 @@ end
 local co1 = coroutine.create(runScript)
 local co2 = coroutine.create(runScript)
 
-coroutine.resume(co1, "")
+coroutine.resume(co1, "https://raw.githubusercontent.com/Youifpg/Noel-noa/refs/heads/main/Mobile")
 coroutine.resume(co2, "")
